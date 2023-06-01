de05a198d650a6d933ca96cf4b3a523c98b7e454
bhargavi <122342419+bhxrg@users.noreply.github.com>
2023-06-01 21:01:13 +0530

    Add files via upload

---
+++ consecutive integer check.c
@@ -0,0 +1,21 @@
+//consecutive integer checking program
+#include <stdio.h>
+#include<conio.h>
+int main()
+{
+int m,n,t,gcd;
+printf("enter the numbers \n");
+scanf("%d%d",&m,&n);
+t=m>=n?m:n;
+while(t>0)
+{
+  if(m%t==0 && n%t==0)
+  {
+   gcd=t;
+   break;
+  }
+  else
+  t=t-1;
+}
+printf("the gcd of two numbers is %d",gcd);
+}
